66b71363cb214eb07c9f14d9df44ec34bff80ea0
Marian <69646823+Marian06@users.noreply.github.com>
2021-03-14 10:39:46 +0100

    🗑️ Removed some unnecessary code

--- src/main/java/com/myra/dev/marian/commands/music/MusicVoteListener.java
+++ src/main/java/com/myra/dev/marian/commands/music/MusicVoteListener.java
@@ -28,61 +28,57 @@ public class MusicVoteListener {
         final Emote emote = Utilities.getUtils().getEmote("greenTick"); // Get vote reaction emote
         if (!event.getReaction().getReactionEmote().getEmote().equals(emote)) return;
 
-        event.retrieveMessage().queue(
-                message -> { // Retrieve message
-                    event.retrieveMember().queue(member -> {
-                        if (member.getVoiceState() == null) return; // Message author isn't in a vc anymore
-                        if (!member.getVoiceState().inVoiceChannel()) return; // Reaction author isn't in a vc
+        event.retrieveMessage().queue(message -> { // Retrieve message
+            event.retrieveMember().queue(member -> {
+                if (member.getVoiceState() == null) return; // Message author isn't in a vc anymore
+                if (!member.getVoiceState().inVoiceChannel()) return; // Reaction author isn't in a vc
 
-                        final VoiceChannel voiceChannel = event.getMember().getVoiceState().getChannel(); // Get voice channel
-                        if (voiceChannel.getMembers().isEmpty()) return; // Everyone left the voice call
-                        final List<Member> members = voiceChannel.getMembers(); // Get members in a voice channel
-                        final int size = (int) members.stream().filter(vcaller -> !vcaller.getUser().isBot()).count();
+                final VoiceChannel voiceChannel = event.getMember().getVoiceState().getChannel(); // Get voice channel
+                if (voiceChannel.getMembers().isEmpty()) return; // Everyone left the voice call
+                final List<Member> members = voiceChannel.getMembers(); // Get members in a voice channel
+                final int size = (int) members.stream().filter(vcaller -> !vcaller.getUser().isBot()).count();
 
-                        AtomicInteger votes = new AtomicInteger();
-                        message.getReactions().forEach(reaction -> {
-                            // Right emote
-                            if (reaction.getReactionEmote().getEmote().equals(emote)) {
-                                reaction.retrieveUsers().forEachAsync(user -> {
-                                    // Add 1 vote
-                                    if (members.stream().anyMatch(reactedMember -> reactedMember.getUser() == user) && !user.isBot())
-                                        votes.addAndGet(1);
+                AtomicInteger votes = new AtomicInteger();
+                message.getReactions().forEach(reaction -> {
+                    // Right emote
+                    if (reaction.getReactionEmote().getEmote().equals(emote)) {
+                        reaction.retrieveUsers().forEachAsync(user -> {
+                            // Add 1 vote
+                            if (members.stream().anyMatch(reactedMember -> reactedMember.getUser() == user) && !user.isBot())
+                                votes.addAndGet(1);
 
-                                    return true; // Iterates over all entities until the provided action returns false
-                                }).whenComplete((input, exception) -> { // Run after all entities are iterated
-                                    if (exception != null) {
-                                        exception.printStackTrace();
-                                    } else {
-                                        try {
-                                            System.out.println(votes.get());
-                                            if (votes.get() < size / 2) return; // Not enough votes
+                            return true; // Iterates over all entities until the provided action returns false
+                        }).whenComplete((input, exception) -> { // Run after all entities are iterated
+                            if (exception != null) {
+                                exception.printStackTrace();
+                            } else {
+                                try {
+                                    System.out.println(votes.get());
+                                    if (votes.get() < size / 2) return; // Not enough votes
 
-                                            final String prefix = new Database(event.getGuild()).getString("prefix"); // Get prefix
-                                            final TrackScheduler scheduler = PlayerManager.getInstance().getMusicManager(event.getGuild()).scheduler;  // Get track scheduler
+                                    final String prefix = new Database(event.getGuild()).getString("prefix"); // Get prefix
+                                    final TrackScheduler scheduler = PlayerManager.getInstance().getMusicManager(event.getGuild()).scheduler;  // Get track scheduler
 
-                                            final List<String> skipExecutors = Myra.COMMAND_SERVICE.getCommandExecutors(MusicSkip.class); /** Get all executors from the {@link MusicSkip} class*/
-                                            final List<String> clearQueueExecutors;
-                                            clearQueueExecutors = Myra.COMMAND_SERVICE.getCommandExecutors(MusicClearQueue.class); /** Get all executors from the {@link MusicClearQueue} class*/
+                                    final List<String> skipExecutors = Myra.COMMAND_SERVICE.getCommandExecutors(MusicSkip.class); /** Get all executors from the {@link MusicSkip} class*/
+                                    final List<String> clearQueueExecutors;
+                                    clearQueueExecutors = Myra.COMMAND_SERVICE.getCommandExecutors(MusicClearQueue.class); /** Get all executors from the {@link MusicClearQueue} class*/
 
-                                            // Skip song
-                                            if (skipExecutors.stream().anyMatch(executor -> message.getContentRaw().equalsIgnoreCase(prefix + executor))) {
-                                                musicSkip.skip(event.getGuild(), event.getChannel(), message.getAuthor());
-                                            }
-                                            // Clear queue
-                                            if (clearQueueExecutors.stream().anyMatch(executor -> message.getContentRaw().equalsIgnoreCase(prefix + executor))) {
-                                                musicClearQueue.clearQueue(scheduler, event.getChannel(), message.getAuthor());
-                                            }
-                                        } catch (Exception e) {
-                                            e.printStackTrace();
-                                        }
+                                    // Skip song
+                                    if (skipExecutors.stream().anyMatch(executor -> message.getContentRaw().equalsIgnoreCase(prefix + executor))) {
+                                        musicSkip.skip(event.getGuild(), event.getChannel(), message.getAuthor());
                                     }
-                                });
+                                    // Clear queue
+                                    if (clearQueueExecutors.stream().anyMatch(executor -> message.getContentRaw().equalsIgnoreCase(prefix + executor))) {
+                                        musicClearQueue.clearQueue(scheduler, event.getChannel(), message.getAuthor());
+                                    }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
                             }
                         });
-                    });
-                },
-                error -> {
-
+                    }
                 });
+            });
+        });
     }
 }
