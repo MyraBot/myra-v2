79ec427837eafd49eaec5b6bab5ad9730da1c2b7
Marian <69646823+Marian06@users.noreply.github.com>
2021-05-09 16:00:54 +0200

    ✏️ Fixed typo

--- src/main/java/com/myra/dev/marian/commands/help/InviteThanks.java
+++ src/main/java/com/myra/dev/marian/commands/help/InviteThanks.java
@@ -14,7 +14,7 @@ public class InviteThanks {
                 final EmbedBuilder thank = new EmbedBuilder()
                         .setAuthor("Thank you for inviting me", null, event.getJDA().getSelfUser().getEffectiveAvatarUrl())
                         .setColor(Utilities.getUtils().blue)
-                        .setDescription("Thank you for inviting me to " + event.getGuild().getName() + ". I'm still in developing, so if you find any bugs please report it! For suggestions you can also join the " + Utilities.getUtils().hyperlink("support server", "https://discord.gg/nG4uKuB") + ".");
+                        .setDescription("Thank you for inviting me to " + event.getGuild().getName() + ". I'm still in development, so if you find any bugs please report it! For suggestions you can also join the " + Utilities.getUtils().hyperlink("support server", "https://discord.gg/nG4uKuB") + ".");
                 privateChannel.sendMessage(thank.build()).queue();
             });
         });
