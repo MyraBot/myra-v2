4c531a50397f914aa7520a0efc50edbefc2d74a1
Marian <69646823+Marian06@users.noreply.github.com>
2021-05-09 16:00:39 +0200

    🩹 Fixed non-retrievable owners

--- src/main/java/com/myra/dev/marian/marian/ServerTracking.java
+++ src/main/java/com/myra/dev/marian/marian/ServerTracking.java
@@ -10,32 +10,45 @@ import net.dv8tion.jda.api.events.guild.GuildLeaveEvent;
 public class ServerTracking {
 
     public void onGuildJoin(GuildJoinEvent event) {
-        event.getGuild().retrieveOwner().queue(owner -> {
-            EmbedBuilder server = new EmbedBuilder()
-                    .setTitle("I joined " + event.getGuild().getName())
-                    .setColor(Utilities.getUtils().blue)
-                    .setThumbnail(event.getGuild().getIconUrl())
-                    .addField("\uD83D\uDC51 │ owner ", owner.getUser().getAsTag(), true)
-                    .addField("\uD83C\uDF9F │ guild id ", event.getGuild().getId(), true)
-                    .addField("\uD83E\uDDEE │ member count", Integer.toString(event.getGuild().getMemberCount()), true)
-                    .setTimestamp(event.getGuild().getMember(event.getJDA().getSelfUser()).getTimeJoined().toInstant());
+        EmbedBuilder server = new EmbedBuilder()
+                .setTitle("I joined " + event.getGuild().getName())
+                .setColor(Utilities.getUtils().blue)
+                .setThumbnail(event.getGuild().getIconUrl())
+                .setTimestamp(event.getGuild().getMember(event.getJDA().getSelfUser()).getTimeJoined().toInstant());
 
-            event.getJDA().getGuildById(Config.marianServer).getTextChannelById("788448343927160852").sendMessage(server.build()).queue();
-        });
+        event.getGuild().retrieveOwner().queue(
+                owner -> {
+                    server.addField("\uD83D\uDC51 │ owner ", owner.getUser().getAsTag(), true)
+                            .addField("\uD83C\uDF9F │ guild id ", event.getGuild().getId(), true)
+                            .addField("\uD83E\uDDEE │ member count", Integer.toString(event.getGuild().getMemberCount()), true);
+                    event.getJDA().getGuildById(Config.marianServer).getTextChannelById("788448343927160852").sendMessage(server.build()).queue();
+                }, error -> {
+                    server.addField("\uD83D\uDC51 │ owner ", "unknown", true)
+                            .addField("\uD83C\uDF9F │ guild id ", event.getGuild().getId(), true)
+                            .addField("\uD83E\uDDEE │ member count", Integer.toString(event.getGuild().getMemberCount()), true);
+
+                    event.getJDA().getGuildById(Config.marianServer).getTextChannelById("788448343927160852").sendMessage(server.build()).queue();
+                });
     }
 
     public void onGuildLeave(GuildLeaveEvent event) {
-        event.getGuild().retrieveOwner().queue(owner -> {
-            EmbedBuilder server = new EmbedBuilder()
-                    .setTitle("I got kicked out of " + event.getGuild().getName())
-                    .setColor(Utilities.getUtils().red)
-                    .setThumbnail(event.getGuild().getIconUrl())
-                    .addField("\uD83D\uDC51 │ owner ", owner.getUser().getAsTag(), true)
-                    .addField("\uD83C\uDF9F │ guild id ", event.getGuild().getId(), true)
-                    .addField("\uD83E\uDDEE │ member count", Integer.toString(event.getGuild().getMemberCount()), true)
-                    .setTimestamp(event.getGuild().getMember(event.getJDA().getSelfUser()).getTimeJoined().toInstant());
+        EmbedBuilder server = new EmbedBuilder()
+                .setTitle("I got kicked out of " + event.getGuild().getName())
+                .setColor(Utilities.getUtils().red)
+                .setThumbnail(event.getGuild().getIconUrl())
+                .setTimestamp(event.getGuild().getMember(event.getJDA().getSelfUser()).getTimeJoined().toInstant());
 
-            event.getJDA().getGuildById(Config.marianServer).getTextChannelById("788448343927160852").sendMessage(server.build()).queue();
-        });
+        event.getGuild().retrieveOwner().queue(
+                owner -> {
+                    server.addField("\uD83D\uDC51 │ owner ", owner.getUser().getAsTag(), true)
+                            .addField("\uD83C\uDF9F │ guild id ", event.getGuild().getId(), true)
+                            .addField("\uD83E\uDDEE │ member count", Integer.toString(event.getGuild().getMemberCount()), true);
+                    event.getJDA().getGuildById(Config.marianServer).getTextChannelById("788448343927160852").sendMessage(server.build()).queue();
+                }, error -> {
+                    server.addField("\uD83D\uDC51 │ owner ", "unknown", true)
+                            .addField("\uD83C\uDF9F │ guild id ", event.getGuild().getId(), true)
+                            .addField("\uD83E\uDDEE │ member count", Integer.toString(event.getGuild().getMemberCount()), true);
+                    event.getJDA().getGuildById(Config.marianServer).getTextChannelById("788448343927160852").sendMessage(server.build()).queue();
+                });
     }
 }
