91e54d4acda879eb6d6b105d2a69a9ef2fe47e4e
Marian <69646823+Marian06@users.noreply.github.com>
2021-06-15 19:29:49 +0200

    📋 Added command info

--- src/main/java/com/github/m5rian/myra/commands/member/economy/EconomyHelp.java
+++ src/main/java/com/github/m5rian/myra/commands/member/economy/EconomyHelp.java
@@ -4,9 +4,8 @@ import com.github.m5rian.jdaCommandHandler.Channel;
 import com.github.m5rian.jdaCommandHandler.CommandContext;
 import com.github.m5rian.jdaCommandHandler.CommandEvent;
 import com.github.m5rian.jdaCommandHandler.CommandHandler;
-import com.github.m5rian.myra.utilities.EmbedMessage.CommandUsage;
-import com.github.m5rian.myra.utilities.EmbedMessage.Usage;
-import com.github.m5rian.myra.utilities.language.Lang;
+import com.github.m5rian.myra.commands.member.economy.administrator.Currency;
+import com.github.m5rian.myra.commands.member.economy.administrator.EconomySet;
 import com.github.m5rian.myra.utilities.permissions.Administrator;
 
 public class EconomyHelp implements CommandHandler {
@@ -22,17 +21,9 @@ public class EconomyHelp implements CommandHandler {
         // Check for no arguments
         if (ctx.getArguments().length == 0) {
             // Send command usages
-            new CommandUsage(ctx.getEvent())
-                    .setCommand("notifications")
-                    .addUsages(
-                            new Usage()
-                                    .setUsage("economy set <user> <balance>")
-                                    .setEmoji("\uD83D\uDC5B")
-                                    .setDescription(Lang.lang(ctx).get("description.economySet")),
-                            new Usage()
-                                    .setUsage("economy currency <currency>")
-                                    .setEmoji("new MongoGuild(ctx.getGuild()).getNested(\"economy\").getString(\"currency\")")
-                                    .setDescription(Lang.lang(ctx).get("description.economy.currency")))
+            usage(ctx).addUsages(
+                    EconomySet.class,
+                    Currency.class)
                     .send();
         }
     }
