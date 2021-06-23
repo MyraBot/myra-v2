9beb3b49a3d8ba00ec386af16642537d4f3885db
Marian <69646823+Marian06@users.noreply.github.com>
2021-06-23 17:21:36 +0200

    :zap: Show now only the rest ping

--- src/main/java/com/github/m5rian/myra/commands/member/help/Ping.java
+++ src/main/java/com/github/m5rian/myra/commands/member/help/Ping.java
@@ -3,8 +3,8 @@ package com.github.m5rian.myra.commands.member.help;
 import com.github.m5rian.jdaCommandHandler.CommandContext;
 import com.github.m5rian.jdaCommandHandler.CommandEvent;
 import com.github.m5rian.jdaCommandHandler.CommandHandler;
-import com.github.m5rian.myra.utilities.EmbedMessage.Success;
 import com.github.m5rian.myra.utilities.language.Lang;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 
 public class Ping implements CommandHandler {
 
@@ -15,28 +15,12 @@ public class Ping implements CommandHandler {
             description = "description.help.ping"
     )
     public void execute(CommandContext ctx) throws Exception {
-        final long time = System.currentTimeMillis();
-        final Success ping = new Success(ctx.getEvent())
-                .setCommand("ping")
-                .setEmoji("\uD83C\uDFD3");
-
-        ctx.getChannel().sendMessage(ping
-                .setMessage(Lang.lang(ctx).get("command.help.ping.pong"))
-                .getEmbed()
-                .build()).queue(message -> {
-            final long commandPing = time - ctx.getEvent().getMessage().getTimeCreated().toInstant().toEpochMilli();
-            final long apiPing = System.currentTimeMillis() - time;
-            // Ping
-            ping.addInlineField("\uD83D\uDD29 │ " + Lang.lang(ctx).get("command.help.ping.ping"),
-                    Lang.lang(ctx).get("command.help.ping.ping.value")
-                            .replace("{$latency}", String.valueOf(commandPing))); // Ping
-            // Discord ping
-            ping.addInlineField("\uD83D\uDD2D │ " + Lang.lang(ctx).get("command.help.ping.apiPing"),
-                    Lang.lang(ctx).get("command.help.ping.apiPing.value")
-                            .replace("{$latency}", String.valueOf(apiPing))); // Api ping
-
-            message.editMessage(ping.getEmbed().build()).queue(); // Edit message
+        ctx.getBot().getRestPing().queue(ping -> {
+            info(ctx).addField(new MessageEmbed.Field(
+                    "\uD83D\uDD29 │ " + Lang.lang(ctx).get("command.help.ping.ping"),
+                    Lang.lang(ctx).get("command.help.ping.ping.value").replace("{$latency}", Long.toString(ping)),
+                    false))
+                    .send();
         });
-
     }
 }
