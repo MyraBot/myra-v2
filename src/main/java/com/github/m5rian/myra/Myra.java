65d9b6e6a1b06be8d6e46212ab9c334c2c1f1a98
Marian <69646823+Marian06@users.noreply.github.com>
2021-06-24 18:43:36 +0200

    :mute: Removed dependency logging on startup

--- src/main/java/com/github/m5rian/myra/Myra.java
+++ src/main/java/com/github/m5rian/myra/Myra.java
@@ -1,18 +1,5 @@
 package com.github.m5rian.myra;
 
-import com.github.m5rian.jdaCommandHandler.commandServices.ICommandService;
-import com.github.m5rian.myra.utilities.Utilities;
-import com.github.natanbc.lavadsp.Converter;
-import com.mongodb.client.MongoDatabase;
-import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
-import net.dv8tion.jda.api.JDA;
-import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.slf4j.Logger;
-import spark.Spark;
-
-import java.util.Properties;
-
 /**
  * @author Marian
  * <p>
@@ -26,21 +13,6 @@ public class Myra {
      * @param args Non-required {@link String} arguments.
      */
     public static void main(String[] args) {
-        // Startup
-        final Properties properties = Utilities.getProperties(); // Get properties
-        System.out.println(properties.getProperty("ascii"));
-        String versions = Utilities.getVersions("\033[0;92m%-25s%s\033[0m\n",
-                Myra.class,
-                JDA.class,
-                ICommandService.class,
-                AudioPlayer.class,
-                Converter.class,
-                MongoDatabase.class,
-                JSONObject.class,
-                Jsoup.class,
-                Spark.class,
-                Logger.class);
-        System.out.println(versions);
         Config.setup();
 
         new Thread(DiscordBot::new, "Discord bot").start(); // Start Discord bot
