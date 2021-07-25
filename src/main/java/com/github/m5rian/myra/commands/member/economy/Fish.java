a5a7a14a1e72c896685101816a5209cb78b097ba
Marian <69646823+Marian06@users.noreply.github.com>
2021-07-25 11:14:31 +0200

    🔨 Remade fish + working now with translations

--- src/main/java/com/github/m5rian/myra/commands/member/economy/Fish.java
+++ src/main/java/com/github/m5rian/myra/commands/member/economy/Fish.java
@@ -5,19 +5,29 @@ import com.github.m5rian.jdaCommandHandler.CommandContext;
 import com.github.m5rian.jdaCommandHandler.CommandEvent;
 import com.github.m5rian.jdaCommandHandler.CommandHandler;
 import com.github.m5rian.myra.Config;
+import com.github.m5rian.myra.database.guild.MongoGuild;
 import com.github.m5rian.myra.database.guild.member.GuildMember;
-import com.github.m5rian.myra.utilities.CommandCooldown;
-import com.github.m5rian.myra.utilities.EmbedMessage.Error;
-import com.github.m5rian.myra.utilities.Utilities;
 import com.github.m5rian.myra.utilities.language.Lang;
-import net.dv8tion.jda.api.EmbedBuilder;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+
+import static com.github.m5rian.myra.utilities.language.Lang.lang;
 
 public class Fish implements CommandHandler {
-    private final int maxCatch = 25;
+    private final int winChance = 60; // In percent
+    private final int looseAmount = 25; // Maximum amount to loose
+    private final List<FishSpecies> fish = new ArrayList<>() {{
+        add(new FishSpecies("\uD83D\uDC1F", 5)); // 🐟
+        add(new FishSpecies("\uD83D\uDC20", 10)); // 🐠
+        add(new FishSpecies("\uD83D\uDC21", 10)); //🐡
+        add(new FishSpecies("\uD83C\uDF8F", 15)); // 🎏
+        add(new FishSpecies("\uD83E\uDD88", 30)); // 🦈
+    }};
+    private final int maxCatch = fish.stream().max(Comparator.comparing(FishSpecies::price)).get().price();
+    private final int looseMessages = 5; // Amount of loose messages
+
+    private record FishSpecies(String emoji, Integer price) {
+    }
 
     @CommandEvent(
             name = "fish",
@@ -28,67 +38,39 @@ public class Fish implements CommandHandler {
     public void execute(CommandContext ctx) throws Exception {
         if (ctx.getArguments().length != 0) return; // Check for no arguments
 
-        if (!CommandCooldown.getInstance().addCommand(ctx, "fish", 5)) return; //Check for cooldown
         final GuildMember db = GuildMember.get(ctx.getMember()); // Get Member in database
+        final String currency = new MongoGuild(ctx.getGuild()).getNested("economy").getString("currency"); // Get guild currency
+
         // Balance limit would be reached
         if (db.getBalance() + maxCatch > Config.ECONOMY_MAX) {
-            new Error(ctx.getEvent())
-                    .setCommand("fish")
-                    .setEmoji("\uD83C\uDFA3")
-                    .setMessage(Lang.lang(ctx).get("command.economy.fish.balanceLimit"))
-                    .send();
+            error(ctx).setDescription(Lang.lang(ctx).get("command.economy.fish.balanceLimit")).send();
             return;
         }
 
         // Caught a fish
-        if (new Random().nextInt(25) <= 20) {
-            final String message = winMessage().get(0); // Get win message
-            final int reward = Integer.parseInt(winMessage().get(1)); // Get price
-            // Send message
-            ctx.getChannel().sendMessage(new EmbedBuilder()
-                    .setAuthor("fish", null, ctx.getAuthor().getEffectiveAvatarUrl())
-                    .setColor(Utilities.blue)
-                    .setDescription("\uD83C\uDFA3 │ " + message + " **+ " + reward + "**")
-                    .build())
-                    .queue();
-            db.setBalance(db.getBalance() + reward); // Update balance
+        if (new Random().nextInt(100) <= winChance) {
+            final int randomFish = new Random().nextInt(fish.size()); // Get random number
+            final FishSpecies caughtFish = fish.get(randomFish); // Get caught fish
+
+            info(ctx).setDescription("\uD83C\uDFA3 " + lang(ctx).get("command.economy.fish.info.caught")
+                    .replace("{$fish}", caughtFish.emoji()) // Fish emoji
+                    .replace("{$price}", String.valueOf(caughtFish.price())) // Price of fish
+                    .replace("{$currency}", currency)) // Guild currency
+                    .send();
+            db.setBalance(db.getBalance() + caughtFish.price()); // Update balance
         }
         // Didn't catch a fish
         else {
-            final String message = loseMessage().get(0); // Get loose message
-            final int lostMoney = Integer.parseInt(loseMessage().get(1)); // Get price
+            final int price = new Random().nextInt(looseAmount);
+            final int randomLooseMessage = new Random().nextInt(looseMessages - 1);
+            final String looseMessage = lang(ctx).get("command.economy.fish.info.loose.0" + randomLooseMessage);
             // Send message
-            ctx.getChannel().sendMessage(new EmbedBuilder()
-                    .setAuthor("fish", null, ctx.getAuthor().getEffectiveAvatarUrl())
-                    .setColor(Utilities.blue)
-                    .setDescription("\uD83C\uDFA3 │ " + message + " **- " + lostMoney + "**")
-                    .build())
-                    .queue();
-            // Get current balance
-            final int balance = db.getBalance();
-            // Update balance
-            db.setBalance(balance - lostMoney);
+            info(ctx).setDescription("\uD83C\uDFA3 " + looseMessage + lang(ctx).get("command.economy.fish.info.loose.money")
+                    .replace("{$price}", String.valueOf(price)) // Price of fish
+                    .replace("{$currency}", currency)) // Guild currency
+                    .send();
+            db.setBalance(db.getBalance() - price); // Update balance
         }
     }
 
-
-    private List<String> winMessage() {
-        // Set messages                         🐟                           🐠                            🐡                           🐚                           🦀                             🦞                          🦐
-        String[] messages = {"You caught a \uD83D\uDC1F", "You caught a \uD83D\uDC20", "You caught a \uD83D\uDC21", "You caught a \uD83D\uDC1A", "You caught a \uD83E\uDD80", "You caught a \uD83E\uDD9E", "You caught a \uD83E\uDD90"};
-        Integer[] rewards = {5, 7, 4, 2, 4, 5, 6};
-        // Get random number
-        Integer random = new Random().nextInt(messages.length);
-        // Return the message with the reward
-        return Arrays.asList(messages[random], rewards[random].toString());
-    }
-
-    private List<String> loseMessage() {
-        // Set messages                                             🐋                🐬                                                                                                  🦈                🐙
-        String[] messages = {"Your line broke while catching a \uD83D\uDC0B", "A \uD83D\uDC2C played around with the line and you were thrown into the water", "You got bitten by a \uD83E\uDD88", "A \uD83D\uDC19 splashed your eyes and you can no longer see anything"};
-        Integer[] rewards = {15, 10, 5, 20};
-        // Get random number
-        Integer random = new Random().nextInt(messages.length);
-        // Return the message with the reward
-        return Arrays.asList(messages[random], rewards[random].toString());
-    }
 }
