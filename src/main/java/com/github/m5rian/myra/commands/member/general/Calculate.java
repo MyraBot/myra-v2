dc853edbace10e45aff17ad2f2465191954c6cd7
Marian <69646823+Marian06@users.noreply.github.com>
2021-07-29 11:05:42 +0200

    📝 Made custom, improved calculator

--- src/main/java/com/github/m5rian/myra/commands/member/general/Calculate.java
+++ src/main/java/com/github/m5rian/myra/commands/member/general/Calculate.java
@@ -1,66 +1,36 @@
 package com.github.m5rian.myra.commands.member.general;
 
 
+import com.github.m5rian.jdaCommandHandler.CommandHandler;
 import com.github.m5rian.jdaCommandHandler.command.CommandContext;
 import com.github.m5rian.jdaCommandHandler.command.CommandEvent;
-import com.github.m5rian.jdaCommandHandler.CommandHandler;
-import com.github.m5rian.myra.utilities.EmbedMessage.CommandUsage;
-import com.github.m5rian.myra.utilities.EmbedMessage.Error;
-import com.github.m5rian.myra.utilities.EmbedMessage.Usage;
-import com.github.m5rian.myra.utilities.EmbedMessage.Success;
 
-import static com.github.m5rian.myra.utilities.language.Lang.*;
+import static com.github.m5rian.myra.utilities.language.Lang.lang;
 
 public class Calculate implements CommandHandler {
 
     @CommandEvent(
             name = "calculate",
             aliases = {"cal"},
-            args = {"<number 1>", "<operator>", "<number 2>"},
+            args = {"<calculation>"},
             emoji = "\uD83E\uDDEE",
             description = "description.general.calculate"
     )
     public void execute(CommandContext ctx) throws Exception {
         // Command usage
-        if (ctx.getArguments().length == 0 || ctx.getArguments().length > 3) {
-            new CommandUsage(ctx.getEvent())
-                    .setCommand("format")
-                    .addUsages(new Usage()
-                            .setUsage("calculate <number> <operator> <number>")
-                            .setEmoji("\uD83E\uDDEE")
-                            .setDescription(lang(ctx).get("description.general.calculate")))
-                    .send();
+        if (ctx.getArguments().length == 0) {
+            usage(ctx).send();
             return;
         }
 
-        // Calculate
         try {
-            final double number1 = Double.parseDouble(ctx.getArguments()[0].replace(",", "."));
-            final double number2 = Double.parseDouble(ctx.getArguments()[2].replace(",", "."));
-            double result = switch (ctx.getArguments()[1]) {
-                case "+" -> number1 + number2;
-                case "-" -> number1 - number2;
-                case "*", "⋅", "x" -> number1 * number2;
-                case "/", ":" -> number1 / number2;
-                default -> 0;
-            };
-
-            new Success(ctx.getEvent())
-                    .setCommand("calculate")
-                    .setEmoji("\uD83E\uDDEE")
-                    .setMessage(lang(ctx).get("command.general.calculate.result")
-                            .replace("{$calculation}", ctx.getArguments()[0] + " " + ctx.getArguments()[1].replace("*", "⋅").replace("x", "⋅").replace("/", ":") + " " + ctx.getArguments()[2])
-                            .replace("{$result}", String.valueOf(result)))
+            final Integer calculation = Calculator.calculate(ctx.getArgumentsRaw().replaceAll("\\s+", ""));
+            info(ctx).setDescription(lang(ctx).get("command.general.calculate.result")
+                    .replace("{$calculation}", ctx.getArgumentsRaw().replaceAll("\\s+", ""))
+                    .replace("{$result}", String.valueOf(calculation)))
                     .send();
-        }
-        // Error occurred
-        catch (Exception e) {
-            e.printStackTrace();
-            new Error(ctx.getEvent())
-                    .setCommand("calculate")
-                    .setEmoji("\uD83E\uDDEE")
-                    .setMessage(lang(ctx).get("command.general.calculate.error"))
-                    .send();
+        } catch (IllegalArgumentException e) {
+            error(ctx).setDescription(lang(ctx).get("command.general.calculate.error")).send();
         }
     }
 }
---
+++ src/main/java/com/github/m5rian/myra/commands/member/general/Calculator.java
@@ -0,0 +1,149 @@
+package com.github.m5rian.myra.commands.member.general;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Calculate a operation with multiple operators.
+ *
+ * @author Marian
+ */
+public class Calculator {
+
+    private static final char[] operatorsToEscape = new char[]{'+', '-', '*', '/'};
+    private static final List<List<Operator>> operationOrder = new ArrayList<>() {{
+        add(new ArrayList<>() {{
+            add(Operator.MULTIPLY);
+            add(Operator.DIVIDE);
+        }});
+        add(new ArrayList<>() {{
+            add(Operator.ADD);
+            add(Operator.SUBTRACT);
+        }});
+    }};
+
+    public static Integer calculate(String operation) throws IllegalArgumentException {
+        if (operation.matches(".*[a-zA-Z].*")) throw new IllegalArgumentException();
+
+        if (operation.startsWith("-")) {
+            operation = "0" + operation;
+        }
+
+        final ArrayList<Integer> digits = new ArrayList<>();
+        final ArrayList<Character> operators = new ArrayList<>();
+
+        // Get digits
+        final Pattern digitPattern = Pattern.compile("\\d+"); // Create a pattern for digits
+        final Matcher digitMatcher = digitPattern.matcher(operation); // Find digits in operation
+        // Go through all found digits
+        while (digitMatcher.find()) {
+            final String number = operation.substring(digitMatcher.start(), digitMatcher.end()); // Get number out of the Matching
+            digits.add(Integer.parseInt(number));
+        }
+
+        // Get operators
+        final Pattern operatorPattern = Pattern.compile(Operator.getPattern()); // Create a pattern for operators out of registered operators
+        final Matcher operatorMatcher = operatorPattern.matcher(operation);  // Find operators in operation
+        // Go through all found digits
+        while (operatorMatcher.find()) {
+            char operator = operation.charAt(operatorMatcher.start()); // Get operator out of the Matching
+            operators.add(operator);
+        }
+
+
+        while (digits.size() > 1) {
+            // Do for every operation rule like brackets before multiplication/division before addition/substraction
+            for (List<Operator> operatorsOfRule : operationOrder) {
+                // For all left operators of the calculation
+                for (int i = 0; i < operators.size(); i++) {
+                    final Operator operator = Operator.getOperator(operators.get(i));
+                    if (operatorsOfRule.contains(operator)) {
+                        applyOperation(operators, digits, i, operator);
+                    }
+                }
+
+            }
+        }
+
+        return digits.get(0);
+    }
+
+    private static void applyOperation(List<Character> operators, List<Integer> digits, int i, Operator operator) throws IllegalArgumentException {
+        try {
+            operators.remove(i); // Remove used operator
+            final int result = operator.operationFunction.apply(digits.get(i), digits.get(i + 1)); // Calculate intermediate result
+            digits.set(i, result); // Replace first operator with result
+            digits.remove(i + 1); // Remove second operator
+            i -= 1;
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private enum Operator {
+        ADD('+', new char[0], Integer::sum),
+        SUBTRACT('-', new char[0], (x, y) -> x - y),
+        MULTIPLY('·', new char[]{'x', '*'}, (x, y) -> x * y),
+        DIVIDE(':', new char[]{'/'}, (x, y) -> x / y);
+
+
+        private final char symbol;
+        private final char[] aliases;
+        private final BiFunction<Integer, Integer, Integer> operationFunction;
+
+        Operator(char symbol, char[] aliases, BiFunction<Integer, Integer, Integer> operationFunction) {
+            this.symbol = symbol;
+            this.aliases = aliases;
+            this.operationFunction = operationFunction;
+        }
+
+        /**
+         * @param query The search query.
+         * @return Returns a {@link Operator} which matches the given pattern. If no operator was found this method returns null.
+         */
+        public static Operator getOperator(char query) {
+            final Optional<Operator> optionalOperator = Arrays.stream(values()).filter(operator -> {
+                boolean matches = false;
+
+                if (operator.symbol == query) matches = true; // Check main symbol
+                // Check alias symbols
+                for (int i = 0; i < operator.aliases.length; i++) {
+                    char alias = operator.aliases[i];
+                    if (alias == query) matches = true;
+                }
+
+                return matches;
+            }).findFirst();
+            return optionalOperator.orElse(null);
+        }
+
+        /**
+         * @return Returns a pattern matching the registered operators with their aliases.
+         */
+        public static String getPattern() {
+            final StringBuilder pattern = new StringBuilder("[");
+            for (Operator operator : values()) {
+
+                pattern.append(operator.symbol).append(",");
+                for (char alias : operator.aliases) {
+                    pattern.append(alias).append(",");
+                }
+
+            }
+
+            pattern.deleteCharAt(pattern.length() - 1).append("]");
+            String finalPattern = pattern.toString();
+            for (char operator : operatorsToEscape) {
+                finalPattern = finalPattern.replace(String.valueOf(operator), "\\" + operator);
+            }
+            return finalPattern;
+        }
+
+    }
+
+}
