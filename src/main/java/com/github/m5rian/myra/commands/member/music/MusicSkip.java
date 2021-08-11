9bc9c711884ed1c1a305599b8a08f90708e02a81
Marian <toni_kukoc@gmx.de>
2021-08-11 12:54:40 +0200

    :package: Added static Lang#lang import

--- src/main/java/com/github/m5rian/myra/commands/member/music/MusicSkip.java
+++ src/main/java/com/github/m5rian/myra/commands/member/music/MusicSkip.java
@@ -1,15 +1,14 @@
 package com.github.m5rian.myra.commands.member.music;
 
 import com.github.m5rian.jdaCommandHandler.Channel;
+import com.github.m5rian.jdaCommandHandler.CommandHandler;
 import com.github.m5rian.jdaCommandHandler.command.CommandContext;
 import com.github.m5rian.jdaCommandHandler.command.CommandEvent;
-import com.github.m5rian.jdaCommandHandler.CommandHandler;
+import com.github.m5rian.myra.database.guild.MongoGuild;
 import com.github.m5rian.myra.utilities.APIs.LavaPlayer.PlayerManager;
 import com.github.m5rian.myra.utilities.EmbedMessage.Error;
 import com.github.m5rian.myra.utilities.EmbedMessage.Success;
 import com.github.m5rian.myra.utilities.Utilities;
-import com.github.m5rian.myra.utilities.language.Lang;
-import com.github.m5rian.myra.database.guild.MongoGuild;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
@@ -33,7 +32,7 @@ public class MusicSkip implements CommandHandler {
             new Error(ctx.getEvent())
                     .setCommand("skip")
                     .setEmoji("\u23ED\uFE0F")
-                    .setMessage(Lang.lang(ctx).get("command.music.error.notPlaying"))
+                    .setMessage(lang(ctx).get("command.music.error.notPlaying"))
                     .send();
             return;
         }
@@ -54,7 +53,7 @@ public class MusicSkip implements CommandHandler {
                 .setCommand("skip")
                 .setEmoji("\u23ED\uFE0F")
                 .setAvatar(author.getUser().getEffectiveAvatarUrl())
-                .setMessage(Lang.lang(author).get("command.music.skip.done")
+                .setMessage(lang(author).get("command.music.skip.done")
                         .replace("{$name}", track.getInfo().title) // Title of track
                         .replace("{$url}", track.getInfo().uri)) // Url of track
                 .setChannel(channel)
